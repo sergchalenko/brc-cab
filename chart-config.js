3fa79054804a7f25c047c42d34d8f03e299778d6
Sergii Chalenko <chaluy99@gmail.com>
2021-04-14 20:26:55 +0300

    14.04.2021

--- chart-config.js
+++ chart-config.js
@@ -1,4 +1,10 @@
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('chart-year').getContext('2d');
+var ctx2 = document.getElementById('chart-month').getContext('2d');
+var ctx3 = document.getElementById('chart-fullyear').getContext('2d');
+
+Chart.defaults.font.family = 'Montserrat'
+Chart.defaults.font.size = 10
+
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -13,7 +19,7 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 99, 132, 1)'
             ],
             borderRadius: Number.MAX_VALUE
-        },
+        }, 
         {
             label: 'Використано годин',
             data: [12, 19, 3, 5, 2, 3, 6],
@@ -45,9 +51,98 @@ var myChart = new Chart(ctx, {
         },
 
         layout: {
+            padding: 40
+        }
+        
+    }
+
+});
+
+var fullYear = new Chart(ctx3, {
+    type: 'bar',
+    data: {
+        labels: ['Замовлено', 'Використано'],
+        datasets: [{
+            label: 'Кількість годин по договору',
+            data: [600, 120],
+            backgroundColor: [
+                'rgba(20, 118, 242, 1)',
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderRadius: Number.MAX_VALUE
+        }
+       ]
+    },
+    
+    options: {
+        
+        plugins: {
+            legend: {
+                display: false,
+                labels: {
+                    font: {
+                        family: 'Montserrat'
+                    }
+                }
+            }
+        },
+        scales: {
+            
+            y: {
+                beginAtZero: true
+            }
+            
+        },
+
+        layout: {
             padding: 30
         }
         
     }
 
+});
+
+var month = new Chart(ctx2, {
+    type: 'pie',
+    data: {
+        labels: ['Використано', 'Залишилось'],
+        datasets: [{
+            label: 'Доступні години',
+            backgroundColor: ['rgba(255, 99, 132, 1)', 'rgba(20, 118, 242, 1)'],
+            borderColor: 'white',
+            data: [20, 40],
+            borderWidth: 0,
+            cutout: 40
+        }, ]
+    },
+
+    options: {
+
+        plugins: {
+            tooltip: {
+                displayColors: false
+            },
+
+            legend: {
+            
+                display: true,
+    
+                labels: {
+                    font: {
+                        family: 'Montserrat',
+                        size: 10
+                    }
+                }
+            },
+        },
+
+        
+
+        layout: {
+            padding: 10
+        }
+    }
 });
--- dashboard.js
+++ dashboard.js
@@ -4,16 +4,35 @@ const menuTasks = document.querySelector('.dashboard__sidebar-menu-tasks');
 const dataYear = document.querySelector('.dashboard__data-year');
 const dataTasks = document.querySelector('.dashboard__tasks');
 
-menuMain.addEventListener('click', function() {
-    menuTasks.classList.remove('_active');
-    dataTasks.classList.remove('_active');
-    dataYear.classList.add('_active');
-    menuMain.classList.add('_active')
-})
+const menuItems = document.querySelectorAll('.menu__item')
 
-menuTasks.addEventListener('click', function() {
-    menuMain.classList.remove('_active');
-    menuTasks.classList.add('_active');
-    dataYear.classList.remove('_active');
-    dataTasks.classList.add('_active')
-})
+console.log(menuItems)
+
+// menuMain.addEventListener('click', function() {
+//     menuTasks.classList.remove('_active');
+//     dataTasks.classList.remove('_active');
+//     dataYear.classList.add('_active');
+//     menuMain.classList.add('_active')
+// })
+
+// menuTasks.addEventListener('click', function() {
+//     menuMain.classList.remove('_active');
+//     menuTasks.classList.add('_active');
+//     dataYear.classList.remove('_active');
+//     dataTasks.classList.add('_active')
+// })
+
+menuTasks.addEventListener('click', activateMenu(menuTasks));
+menuMain.addEventListener('click', activateMenu(menuMain));
+
+function activateMenu(item) {
+    menuItems.forEach(function(element) {
+        if (element.classList.contains('_active')) {
+            element.classList.remove('_active')
+        }
+    });
+    item.classList.add('_active')
+};
+
+
+
